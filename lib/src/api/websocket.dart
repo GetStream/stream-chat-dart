7f9b7c8f5ef8001beab0171ed86c2e917749a338
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2020-01-30 11:24:19 +0100

    refactoring and add logger

--- lib/src/api/websocket.dart
+++ lib/src/api/websocket.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:logging/logging.dart';
+
 import 'web_socket_channel_stub.dart'
-  if (dart.library.html) 'web_socket_channel_html.dart'
-  if (dart.library.io) 'web_socket_channel_io.dart';
+    if (dart.library.html) 'web_socket_channel_html.dart'
+    if (dart.library.io) 'web_socket_channel_io.dart';
 import '../models/event.dart';
 import '../models/user.dart';
+
+typedef EventHandler = void Function(Event);
 
 // TODO: improve error path for the connect() method
 // TODO: make sure we pass an error with a stacktrace
@@ -18,47 +22,48 @@ class WebSocket {
   final User user;
   final Map<String, String> connectParams;
   final Map<String, dynamic> connectPayload;
-  final void Function(Event) handler;
+  final EventHandler handler;
+  final Logger logger;
 
-  bool _resolved;
-
-  WebSocket(
+  WebSocket({
     this.baseUrl,
     this.user,
     this.connectParams,
     this.connectPayload,
     this.handler,
-  ) {
-    _resolved = false;
-  }
+    this.logger,
+  });
 
-  Event decodeEvent(String source) {
-    var map = json.decode(source);
-    return Event.fromJson(map);
-  }
+  Event decodeEvent(String source) => Event.fromJson(json.decode(source));
 
   Future<Event> connect() {
-    var completer = Completer<Event>();
-    var qs = Map<String, String>.from(connectParams);
+    final completer = Completer<Event>();
+    final qs = Map<String, String>.from(connectParams);
 
-    var data = Map<String, dynamic>.from(connectPayload);
+    final data = Map<String, dynamic>.from(connectPayload);
+
     data["user_details"] = user.toJson();
-
     qs["json"] = json.encode(data);
 
-    var uri = Uri.https(baseUrl, "connect", qs);
-    var path = uri.toString().replaceFirst("https", "wss");
+    final uri = Uri.https(baseUrl, "connect", qs);
+    final path = uri.toString().replaceFirst("https", "wss");
+
+    logger.info('connecting to $path');
+
+    bool resolved = false;
 
     final channel = connectWebSocket(path);
     channel.stream.listen((data) {
       final event = decodeEvent(data);
-      if (_resolved) {
+      if (resolved) {
         handler(event);
       } else {
-        _resolved = true;
+        resolved = true;
+        logger.info('connection estabilished');
         completer.complete(event);
       }
     }, onError: (error) {
+      logger.severe('error connecting');
       completer.completeError(error);
     });
     return completer.future;
