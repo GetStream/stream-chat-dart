e3bdf6341d34f61f69555c9c19c00a5c60a855c2
Tommaso Barbugli <tbarbugli@gmail.com>
2020-02-02 17:08:36 +0100

    add client.devToken

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -364,6 +364,13 @@ class Client {
     return decode(response.data, EmptyResponse.fromJson);
   }
 
+  String devToken(String userId) {
+    final payload = json.encode({"user_id": userId});
+    final payloadBytes = utf8.encode(payload);
+    final payloadB64 = base64.encode(payloadBytes);
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.$payloadB64.devtoken";
+  }
+
   Channel channel(
     String type, {
     String id,
