e1456b26dca3faea2cb7dd9a7f76f6127fa994ed
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2020-02-12 16:05:47 +0100

    Built example/web
    Branch: master
    Commit: 09a70d9852bd09ca190a7216d1f1f30051f2dc0b
    
    package:peanut 3.5.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,13 +2,14 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "d72329411d85f7cb516c68dd1a1a94f8",
-"/main.dart.js": "22fde73e79129fe8dccccac29c0750b9",
+"/main.dart.js": "8719caaec9ee97eaa752ac3c5d7098b8",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/manifest.json": "f261bcc762edf3ff16cbc3cd488bb01a",
-"/assets/LICENSE": "7eda59454149050f6c584436e8d44f40",
-"/assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+"/assets/LICENSE": "2a098ce591692624d2db09e0ace5ff3e",
+"/assets/AssetManifest.json": "18bb85605817b5b8ca48b5dad63bdc25",
 "/assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
+"/assets/packages/flutter_markdown/assets/logo.png": "67642a0b80f3d50277c44cde8f450e50",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
 
