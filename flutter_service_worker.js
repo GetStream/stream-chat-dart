f3fb2329de90902e740fd28dad350adae2d32d81
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2020-02-12 16:22:24 +0100

    Built web
    Branch: master
    Commit: 562d7ff5249f196ba4fc3f00ccddb5c2c562266b (dirty)
    
    package:peanut 3.5.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "d72329411d85f7cb516c68dd1a1a94f8",
-"/main.dart.js": "d4df375c9b36b297f8a6e02ea136e7c0",
+"/main.dart.js": "98d4f44f15684855ebfc2c53e63caade",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/manifest.json": "f261bcc762edf3ff16cbc3cd488bb01a",
