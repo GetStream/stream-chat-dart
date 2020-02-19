b8bd31e4c25443413add9d46de6df23440348c11
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2020-02-19 10:30:24 +0100

    add missing exports

--- lib/stream_chat.dart
+++ lib/stream_chat.dart
@@ -1,17 +1,25 @@
 library stream_chat;
 
-export './src/api/requests.dart';
+export 'package:logging/src/level.dart';
+
 export './src/api/channel.dart';
 export './src/api/connection_status.dart';
-export './src/models/message.dart';
-export './src/models/attachment.dart';
-export './src/models/reaction.dart';
+export './src/api/requests.dart';
+export './src/api/requests.dart';
 export './src/api/responses.dart';
 export './src/client.dart';
-export './src/models/channel.dart';
-export './src/models/channel_state.dart';
-export './src/models/event.dart';
-export './src/models/user.dart';
-export './src/api/requests.dart';
+export './src/models/action.dart';
 export './src/models/attachment.dart';
-export 'package:logging/logging.dart';
+export './src/models/channel.dart';
+export './src/models/channel_config.dart';
+export './src/models/channel_state.dart';
+export './src/models/command.dart';
+export './src/models/device.dart';
+export './src/models/event.dart';
+export './src/models/member.dart';
+export './src/models/message.dart';
+export './src/models/mute.dart';
+export './src/models/own_user.dart';
+export './src/models/reaction.dart';
+export './src/models/read.dart';
+export './src/models/user.dart';
