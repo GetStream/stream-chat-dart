ca0a7025de8cb6817292b4a4a8b8678a561f96fb
Deven Joshi <deven9852@gmail.com>
2020-09-18 19:22:12 +0530

    fix: WidgetsBinding

--- lib/src/db/shared/native_db.dart
+++ lib/src/db/shared/native_db.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:moor/ffi.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
 class SharedDB {
   static constructDatabase(dbName) async {
+    WidgetsFlutterBinding.ensureInitialized();
     final dir = await getApplicationDocumentsDirectory();
     final path = join(dir.path, dbName);
 
