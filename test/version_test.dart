befd1d5596a96293c8ea517df5fbc937011a7d01
Sahil Kumar <xdsahil@gmail.com>
2020-10-22 00:21:36 +0530

    fix test
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

--- test/version_test.dart
+++ test/version_test.dart
@@ -3,7 +3,15 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:stream_chat/version.dart';
 
+void prepareTest() {
+  // https://github.com/flutter/flutter/issues/20907
+  if (Directory.current.path.endsWith('/test')) {
+    Directory.current = Directory.current.parent;
+  }
+}
+
 void main() {
+  prepareTest();
   test('stream chat version matches pubspec', () {
     final String pubspecPath = '${Directory.current.path}/pubspec.yaml';
     final String pubspec = File(pubspecPath).readAsStringSync();
